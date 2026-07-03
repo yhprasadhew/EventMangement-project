0365104afc660850b7ab3a3708eed02b30cd6a45
yhprasad <yhprasadhew@gmail.com>
2026-07-03 07:34:56 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>hello prasad</h1>
+    <div className= "flex bg-amber-200">prasad</div>
+    </>
+  );
+}
